3ed6a64d7d998cf7da6c40d103dd9d483a38c676
GuztaJF-DS <noobshippuden13@gmail.com>
2021-08-31 13:47:15 -0300

    (Update) Rotation Icon Changed a Bit

--- src/components/productComponents/RotatedIcon.js
+++ src/components/productComponents/RotatedIcon.js
@@ -2,25 +2,26 @@ import React from 'react';
 import { View,Text } from 'react-native';
 
 export default ({Order,Op})=>{
+    var Color="#ebb89b";
     var deg=
     (Op==1)?
         (Order=="3"||Order=="4")?
-            0   
+            Color='#68293f' && 0
         :(Order=="1")?
-        90:-90
+        0:180
     :(Op==2)?
         (Order=="1"||Order=="2")?
-            180
+            Color='#68293f' && 0
         :(Order=="3")?
-            90:-90
+            0:180
     :0
 
     return(
         <View style={{
-            transform: [{ rotate: `${deg}deg` }]
+            transform: [{ rotateZ: `${deg}deg`}]
         }}>
-            <Text style={{color:"#ebb89b"}}>
-                {'>'}
+            <Text style={{color:Color}}>
+                {'▲'}
             </Text>
         </View>
     )
