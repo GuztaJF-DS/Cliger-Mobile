26970794a12c5563bd2058012fcfadc40665b088
GuztaJF-DS <noobshippuden13@gmail.com>
2021-09-28 17:31:38 -0300

    Improved Graphics

--- src/components/finances/LineGraphic.js
+++ src/components/finances/LineGraphic.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { VictoryChart,VictoryVoronoiContainer,VictoryTheme, VictoryLine } from "victory-native";
-import { View } from "react-native"
+import { View } from "react-native";
+import {Chart,Line,Tooltip,VerticalAxis} from 'react-native-responsive-linechart'
 
 export default function Finance({data}){
     var obj=[];
@@ -8,28 +8,19 @@ export default function Finance({data}){
     for (var i in data){
         obj.push({"x":`${parseInt(i)+1}`,"y":data[i].CurrentBalance,"label":""});
     }
-    console.log(obj);
 
     return(
-            <View >
-                <VictoryChart
-                    theme={VictoryTheme.material}
-                    containerComponent={
-                      <VictoryVoronoiContainer
-                        dimension="x"
-                        labels={({datum})=> `${datum.y}`}
-                      />
-                    }
-                    >
-                    <VictoryLine
-                        name="Line"
-                        style={{
-                          data: { stroke: "#c43a31" },
-                          parent: { border: "1px solid #ccc"}
-                        }}
-                        data={obj}
-                    />
-                  </VictoryChart>
+            <View>
+                <Chart
+                  style={{ height: 200, width: 380 }}
+                  data={obj}
+                  padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
+                  xDomain={{ min: 1, max: 10 }}
+                  yDomain={{ min: 0, max: 60 }}
+                >
+                  <VerticalAxis tickCount={7} theme={{labels:{label:{color:'#fff'}}}}/>
+                  <Line tooltipComponent={<Tooltip theme={{ formatter: ({ y }) => y.toFixed(2) }}/>}  theme={{ stroke: { color: '#44bd32', width: 5 },scatter:{default:{width:8,height:8,rx:5,color:'#329923'}}}}/>
+                </Chart>
             </View>
     )
 }
